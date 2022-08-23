6cc37fdd566c5c5bd913dd74a63bc5f0d4f3e207
Gautam <43657587+gautam-ergo@users.noreply.github.com>
2022-08-23 18:24:49 -0500

    Update global.R

--- Sentiment Analysis/global.R
+++ Sentiment Analysis/global.R
@@ -50,10 +50,10 @@ library(shinythemes)
 library(syuzhet)
 
 # Twitter authentication key
-oauth = setup_twitter_oauth(consumer_key = "LhLzIn0nbz5mORcE3wPdSmWjP",
-                            consumer_secret = "xILbs2S5IbNiZyFwXU7VITcVCxxzf3SpA2Gbvn3qBNF8LY8woQ",
-                            access_token = "110651492-aB9iL1exrmkb3Q2gmM2DEqCzz6eo0TQiqqjRRXec",
-                            access_secret = "8XZIQ6eVvOwAwhvGSSIL4SEitrttjAkf6SYAJHFXMFxz9")
+oauth = setup_twitter_oauth(consumer_key = "****",
+                            consumer_secret = "****",
+                            access_token = "****",
+                            access_secret = "***")
 
 cat("\014")
 
